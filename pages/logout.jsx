7be1d86853b5b037b406cef0f0eab56e5dd09709
superTyDev <tysonm2496@mypolkschools.net>
2023-01-10 18:41:35 -0500

    more conversions

--- pages/logout.jsx
+++ pages/logout.jsx
@@ -1,14 +1,9 @@
 import React, { useEffect, useState } from "react";
-
 import { auth, logout } from "../components/fbauth.js";
-import { useAuthState } from "react-firebase-hooks/auth";
 
-export default function Logout() {
-  const [location, setLocation] = useLocation();
-  
-  useEffect(() => {
-    logout();
-    setLocation("/");
-  });
-  return <h1>Logout</h1>;
+export default function Logout(req, res) {
+	logout();
+	res.redirect("/");
+
+	return <h1>Logout</h1>;
 }
