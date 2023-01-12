139faa618f32d5d2c8884421371698298fd72725
superTyDev <tysonm2496@mypolkschools.net>
2023-01-12 11:18:12 +0000

    optimize images/add title

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,13 @@ export default function Document() {
 	return (
 		<Html lang="en">
 			<Head>
+				<title>Altium Aeronautics</title>
+				<meta property="og:title" content="Altium Aeronautics" key="title" />
+
+				<meta charset="UTF-8" />
+				<meta name="keywords" content="space, astronaut" />
+				<meta name="author" content="T M, C A" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<Script src="https://kit.fontawesome.com/b02a1c9014.js"></Script>
 			</Head>
 			<body>
