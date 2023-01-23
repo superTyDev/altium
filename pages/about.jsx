0f6ad44ad46052aa3d0037751a9d84a8a1f2e4b3
superTyDev <tysonm2496@mypolkschools.net>
2023-01-23 18:03:10 -0500

    renaming

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,135 +1,177 @@
 import Image from "next/image";
-import * as React from "react";
+import Link from "next/link";
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-import styles from "../styles/About.module.css"
+import styles from "../styles/Ships.module.css";
 
-let slideIndex = 1;
+export default function Ships() {
+  return (
+    <>
+      <div>
+        <div className={styles.hero}>
+          <div className="navSpacer"></div>
+          <Image
+            className={styles.backgroundImg}
+            src={
+              "https://cdn.glitch.global/d7070554-ac67-4c73-a3d6-aadfe190dab5/sunRise.webp?v=1674255059462"
+            }
+            width={1980}
+            height={500}
+            alt="Sun Rise over Earth from Space"
+          />
+          <h1>Meet the Ships</h1>
+        </div>
 
-// Next/previous controls
-function plusSlides(n) {
-	showSlides(slideIndex += n);
-}
+        <div className={styles.backgroundSection}>
+          <div className={styles.section}>
+            <Carousel
+              className={styles.sectionImage}
+              autoPlay
+              infiniteLoop
+              showThumbs={false}
+            >
+              <div>
+                <img
+                  src="https://cdn.glitch.global/d7070554-ac67-4c73-a3d6-aadfe190dab5/sr726.webp?v=1674353619826"
+                  alt="image3"
+                  loading="lazy"
+                />
+                <p className="legend">Darkhawk in Hanger</p>
+              </div>
+              <div>
+                <img
+                  src="https://cdn.glitch.global/d7070554-ac67-4c73-a3d6-aadfe190dab5/sr724.webp?v=1674353628090"
+                  alt="image1"
+                  loading="lazy"
+                />
+                <p className="legend">Darkhawk at Space</p>
+              </div>
+              <div>
+                <img
+                  src="https://cdn.glitch.global/d7070554-ac67-4c73-a3d6-aadfe190dab5/sr725.webp?v=1674353623764"
+                  alt="image2"
+                  loading="lazy"
+                />
+                <p className="legend">Low Altitude Rendering</p>
+              </div>
+            </Carousel>
+            <div className={styles.altCont}>
+              <h2>SR 72 Darkhawk</h2>
+              <p>
+                The SR 72 Darkhawk is a revolutionary aircraft developed by
+                Lockheed Martin and NASA. It is originally an unmanned, high
+                altitude, supersonic aircraft capable of reaching speeds up to
+                Mach 6. Due to its scram jet engine, it is able to fly to the
+                edge of space in a few hours, making it ideal for space tourism.
+              </p>
+              <div className="buttonCont">
+                <Link href="/quote" className="button">
+                  Book Darkhawk
+                </Link>
+              </div>
+            </div>
+          </div>
 
-// Thumbnail image controls
-function currentSlide(n) {
-	showSlides(slideIndex = n);
-}
+          <div className={styles.section}>
+            <Carousel
+              className={styles.sectionImage}
+              autoPlay
+              infiniteLoop
+              showThumbs={false}
+            >
+              <div>
+                <img
+                  src="https://cdn.glitch.global/d7070554-ac67-4c73-a3d6-aadfe190dab5/modernShuttleShort.webp?v=1674259900863"
+                  alt="image1"
+                  loading="lazy"
+                />
+                <p className="legend">SS Gibraltar in Space</p>
+              </div>
+              <div>
+                <img
+                  src="https://cdn.glitch.global/d7070554-ac67-4c73-a3d6-aadfe190dab5/modernShuttleShort2.webp?v=1674258615375"
+                  alt="image2"
+                  loading="lazy"
+                />
+                <p className="legend">Construction of the Gibraltar</p>
+              </div>
+              <div>
+                <img
+                  src="https://cdn.glitch.global/d7070554-ac67-4c73-a3d6-aadfe190dab5/modernShuttleShort3.webp?v=1674258615375"
+                  alt="image3"
+                  loading="lazy"
+                />
+                <p className="legend">
+                  Gibraltar&Aapos;s Space Launch Vehicle (SLV)
+                </p>
+              </div>
+            </Carousel>
+            <div className={styles.altCont}>
+              <h2>SS Gibraltar</h2>
+              <p>
+                This revolutionary spacecraft is designed to take you to the
+                moon and beyond. With its powerful engines and advanced
+                navigation systems, the Gibraltar is Altium&apos;s medium
+                distance shuttle. This shuttle is optimized for quick trips to
+                the ISS or longer distance flights.
+              </p>
+              <div className="buttonCont">
+                <Link href="/quote" className="button">
+                  Book Gibraltar
+                </Link>
+              </div>
+            </div>
+          </div>
 
-function showSlides(n = 1) {
-	let i;
-	let slides = document.getElementsByClassName(styles.mySlides);
-	let dots = document.getElementsByClassName(styles.demo);
-	let captionText = document.getElementById("caption");
-
-	if (n > slides.length) { slideIndex = 1 }
-	if (n < 1) { slideIndex = slides.length }
-
-	if (slides.length) {
-		for (i = 0; i < slides.length; i++) {
-			slides[i].style.display = "none";
-		}
-		for (i = 0; i < dots.length; i++) {
-			dots[i].classList.remove("active");
-		}
-
-		slides[slideIndex - 1].style.display = "block";
-		dots[slideIndex - 1].classList.add("active");
-		captionText.innerHTML = dots[slideIndex - 1].alt;
-	}
-}
-
-function onPageLoad() {
-	showSlides(slideIndex);
-}
-
-export default function About() {
-	if (typeof window !== "undefined") {
-		if (document.readyState === "complete") {
-			onPageLoad();
-		} else {
-			window.addEventListener("load", onPageLoad);
-		}
-	}
-
-	return (
-		<>
-			<div className="navSpacer"></div>
-			<div>
-				<h1 className="title">About</h1>
-				<div className={styles.container}>
-
-					<div className={styles.mySlides}>
-						<div className={styles.numbertext}>1 / 6</div>
-						<Image src={"https://cdn.glitch.global/d7070554-ac67-4c73-a3d6-aadfe190dab5/final_configuration_of_iss.jpg?v=1673519859930"} width={400} height={400} alt="Final Configuration of ISS" />
-					</div>
-
-					<div className={styles.mySlides}>
-						<div className={styles.numbertext}>2 / 6</div>
-						<Image src={"https://cdn.glitch.global/d7070554-ac67-4c73-a3d6-aadfe190dab5/final_configuration_of_iss.jpg?v=1673519859930"} width={400} height={400} alt="Final Configuration of ISS" />
-					</div>
-
-					<div className={styles.mySlides}>
-						<div className={styles.numbertext}>3 / 6</div>
-						<Image src={"https://cdn.glitch.global/d7070554-ac67-4c73-a3d6-aadfe190dab5/final_configuration_of_iss.jpg?v=1673519859930"} width={400} height={400} alt="Final Configuration of ISS" />
-					</div>
-
-					<div className={styles.mySlides}>
-						<div className={styles.numbertext}>4 / 6</div>
-						<Image src={"https://cdn.glitch.global/d7070554-ac67-4c73-a3d6-aadfe190dab5/final_configuration_of_iss.jpg?v=1673519859930"} width={400} height={400} alt="Final Configuration of ISS" />
-					</div>
-
-					<div className={styles.mySlides}>
-						<div className={styles.numbertext}>5 / 6</div>
-						<Image src={"https://cdn.glitch.global/d7070554-ac67-4c73-a3d6-aadfe190dab5/final_configuration_of_iss.jpg?v=1673519859930"} width={400} height={400} alt="Final Configuration of ISS" />
-					</div>
-
-					<div className={styles.mySlides}>
-						<div className={styles.numbertext}>6 / 6</div>
-						<Image src={"https://cdn.glitch.global/d7070554-ac67-4c73-a3d6-aadfe190dab5/final_configuration_of_iss.jpg?v=1673519859930"} width={400} height={400} alt="Final Configuration of ISS" />
-					</div>
-
-					<a className={styles.prev} onClick={() => plusSlides(-1)}>&#10094;</a>
-					<a className={styles.next} onClick={() => plusSlides(1)}>&#10095;</a>
-
-					<div className={[styles.caption, styles.container].join(" ")}>
-						<p id="caption"></p>
-					</div>
-
-					<div className={styles.row}>
-						<div className={styles.column}>
-							<Image className={[styles.demo, styles.cursor].join(" ")} src={"https://cdn.glitch.global/d7070554-ac67-4c73-a3d6-aadfe190dab5/final_configuration_of_iss.jpg?v=1673519859930"} width={100} height={100} onClick={() => currentSlide(1)} alt="The Woods" />
-						</div>
-						<div className={styles.column}>
-							<Image className={[styles.demo, styles.cursor].join(" ")} src={"https://cdn.glitch.global/d7070554-ac67-4c73-a3d6-aadfe190dab5/final_configuration_of_iss.jpg?v=1673519859930"} width={100} height={100} onClick={() => currentSlide(2)} alt="Cinque Terre" />
-						</div>
-						<div className={styles.column}>
-							<Image className={[styles.demo, styles.cursor].join(" ")} src={"https://cdn.glitch.global/d7070554-ac67-4c73-a3d6-aadfe190dab5/final_configuration_of_iss.jpg?v=1673519859930"} width={100} height={100} onClick={() => currentSlide(3)} alt="Mountains and fjords" />
-						</div>
-						<div className={styles.column}>
-							<Image className={[styles.demo, styles.cursor].join(" ")} src={"https://cdn.glitch.global/d7070554-ac67-4c73-a3d6-aadfe190dab5/final_configuration_of_iss.jpg?v=1673519859930"} width={100} height={100} onClick={() => currentSlide(4)} alt="Northern Lights" />
-						</div>
-						<div className={styles.column}>
-							<Image className={[styles.demo, styles.cursor].join(" ")} src={"https://cdn.glitch.global/d7070554-ac67-4c73-a3d6-aadfe190dab5/final_configuration_of_iss.jpg?v=1673519859930"} width={100} height={100} onClick={() => currentSlide(5)} alt="Nature and sunrise" />
-						</div>
-						<div className={styles.column}>
-							<Image className={[styles.demo, styles.cursor].join(" ")} src={"https://cdn.glitch.global/d7070554-ac67-4c73-a3d6-aadfe190dab5/final_configuration_of_iss.jpg?v=1673519859930"} width={100} height={100} onClick={() => currentSlide(6)} alt="Snowy Mountains" />
-						</div>
-					</div>
-				</div >
-				<div className="card">
-					<div className="left">
-						<Image
-							src={
-								"https://cdn.glitch.global/d7070554-ac67-4c73-a3d6-aadfe190dab5/saturnv-crosssection.png?v=1673518459567"
-							}
-							alt="Saturn V Cross Section"
-							width={250}
-							height={250}
-						/>
-					</div>
-					<div className="right"></div>
-				</div>
-			</div >
-		</>
-	);
+          <div className={styles.section}>
+            <Carousel
+              className={styles.sectionImage}
+              autoPlay
+              infiniteLoop
+              showThumbs={false}
+            >
+              <div>
+                <img
+                  src="https://cdn.glitch.global/d7070554-ac67-4c73-a3d6-aadfe190dab5/redRocket.webp?v=1674258725776"
+                  alt="image1"
+                />
+                <p className="legend">Malta Rainforest Launch</p>
+              </div>
+              <div>
+                <img
+                  src="https://cdn.glitch.global/d7070554-ac67-4c73-a3d6-aadfe190dab5/redRocket2.webp?v=1674258725776"
+                  alt="image2"
+                />
+                <p className="legend">Malta Low Impact Thrusters</p>
+              </div>
+              <div>
+                <img
+                  src="https://cdn.glitch.global/d7070554-ac67-4c73-a3d6-aadfe190dab5/redRocket3.webp?v=1674258725776"
+                  alt="image3"
+                />
+                <p className="legend">Malta in Hanger</p>
+              </div>
+            </Carousel>
+            <div className={styles.altCont}>
+              <h2>STS Malta</h2>
+              <p>
+                Altium&apos;s largest rocket is designed to carry payloads to
+                the moon and beyond. With its state-of-the-art ion trust
+                technology, the STS Malta is the perfect choice for those
+                looking to explore the depths of space. And with its comfortable
+                interior, you can enjoy the journey in style.
+              </p>
+              <div className="buttonCont">
+                <Link href="/quote" className="button">
+                  Book Malta
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
