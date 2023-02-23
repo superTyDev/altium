990110a47c681a5ce3efddaeb80cc2baac168a9e
superTyDev <tysonm2496@mypolkschools.net>
2023-02-22 20:03:44 -0500

    re-added accidentally deleted file/restored training

--- pages/api/lessondata.js
+++ pages/api/lessondata.js
@@ -1,6 +1,6 @@
 import fsPromises from "fs/promises";
 import path from "path";
-export async function getStaticProps() {
+export default async function getStaticProps() {
 	const filePath = path.join(process.cwd(), "json/lessons.json");
 	const jsonData = await fsPromises.readFile(filePath);
 	const objectData = JSON.parse(jsonData);
